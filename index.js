8aeda7d0a1936c543762c6302a365b7f81e0f612
Daniel Glover <iamdanielglover@gmail.com>
2018-11-20 12:46:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  //Write your code here
+ var itemName = item;
  var price = Math.floor(Math.random()*100) + 1;
  cart.push({ [item] : price });
  console.log(`${item} has been added to your cart.`);
